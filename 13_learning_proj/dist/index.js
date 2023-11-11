533718c8639ebccb4bb0c155f06901a1fa5d64de
MaheshRautrao <101188065+MaheshRautrao@users.noreply.github.com>
2023-11-11 18:44:41 +0530

    classes added

--- 13_learning_proj/dist/index.js
+++ 13_learning_proj/dist/index.js
@@ -1,2 +1,36 @@
 "use strict";
 console.log("typescript from src");
+// class User {
+//   email: string;
+//   name: string;
+//   city: string = "";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+// const mahesh = new User("mahesh", "maehsh20@gamil");
+// mahesh.city = "jaipur";
+// access keywords
+// private --> you can use it only in the class
+// by default everthing is public --> you can access anywhere
+// class User {
+//   email: string;
+//   name: string;
+//   private readonly city: string = "";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//     this.city = "jaipur";
+//   }
+// }
+// another way to write is private in constructor only
+// class User {
+//   private readonly city: string = "";
+//   constructor(private email: string, private name: string) {
+//     this.email = email;
+//     this.name = name;
+//     this.city = "jaipur";
+//   }
+// }
+// getters and setters in ts
